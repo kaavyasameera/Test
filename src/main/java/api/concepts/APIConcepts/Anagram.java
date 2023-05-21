6e51b7e9254dbbfea97ab15c720bd6fc149202c9
bvamsib <121925173+bvamsib@users.noreply.github.com>
2023-05-21 14:08:14 +0530

    Update Anagram 6.java

--- src/main/java/api/concepts/APIConcepts/Anagram.java
+++ src/main/java/api/concepts/APIConcepts/Anagram.java
@@ -8,7 +8,7 @@ public class Anagram {
 		String s1 = "aabb", s2 = "Care";
 		s1 = s1.toLowerCase();
 		s2 = s2.toLowerCase();
-		char[] s11 = s1.toCharArray(); //hello world KIm Modi India High
+		char[] s11 = s1.toCharArray(); //hello world KIms
 		char[] s12 = s2.toCharArray(); //tom
 		int len = s1.length();
 		char temp=' ';
