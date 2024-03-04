2c12b3eab6031d9277ff2b37ecfb4fa5671d7c9c
kaavyasameera <sajjakaavya@gmail.com>
2024-03-04 11:44:21 +0530

    commit for replaceall file

--- src/main/java/api/concepts/APIConcepts/ReplaceAll.java
+++ src/main/java/api/concepts/APIConcepts/ReplaceAll.java
@@ -4,7 +4,7 @@ import groovy.transform.ASTTest;
 
 public class ReplaceAll {
     public static void main(String [] args){
-        String name = "kaavya123@#";
+        String name = "kaaggvya123@#";
         name = name.replaceAll("[^a-z]","").toUpperCase();
         System.out.println(name);
     }
