a0db859d83c325a385703d84492277bcba7bf0b1
kaavyasameera <sajjakaavya@gmail.com>
2024-03-05 10:38:59 +0530

    Added print statement

--- src/main/java/api/concepts/APIConcepts/ReplaceAll.java
+++ src/main/java/api/concepts/APIConcepts/ReplaceAll.java
@@ -6,6 +6,6 @@ public class ReplaceAll {
     public static void main(String [] args){
         String name = "vamsi123@#";
         name = name.replaceAll("[^a-z]","").toUpperCase();
-        System.out.println(name);
+        System.out.println("name is in capital letters : "name);
     }
 }
