5a78026a0360aff3a7df619f9ae0e45c973b2bc2
kaavyasameera <sajjakaavya@gmail.com>
2024-03-05 10:56:04 +0530

    name becomes kasvika

--- src/main/java/api/concepts/APIConcepts/ReplaceAll.java
+++ src/main/java/api/concepts/APIConcepts/ReplaceAll.java
@@ -4,7 +4,7 @@ import groovy.transform.ASTTest;
 
 public class ReplaceAll {
     public static void main(String [] args){
-        String name = "KASVIKA123@#";
+        String name = "kasvika123@#";
         name = name.replaceAll("[^a-z]","").toLowerCase();
         System.out.println("name is in small letters : "name);
     }
