778fe142ff6a79371b4e3ed60ef18ca1b13fdad0
kaavyasameera <162018458+kaavyasameera@users.noreply.github.com>
2024-03-05 11:07:35 +0530

    Update ReplaceAll.java
    print statement becomes name is

--- src/main/java/api/concepts/APIConcepts/ReplaceAll.java
+++ src/main/java/api/concepts/APIConcepts/ReplaceAll.java
@@ -6,7 +6,7 @@ public class ReplaceAll {
     public static void main(String [] args){
         String name = "kasvika123@#";
         name = name.replaceAll("[^a-z]","").toUpperCase();
-        System.out.println("name is in capital letters : "name);
+        System.out.println("name is  : "name);
 
     }
 }
