ad3ba409d6daeaf2030317825c2749233ba0a81d
kaavyasameera <162018458+kaavyasameera@users.noreply.github.com>
2024-03-05 10:53:56 +0530

    Story kaavya2 (#5)
    * added name as KASVIKA
    
    * to lowercase and small letters in print statement.

--- src/main/java/api/concepts/APIConcepts/ReplaceAll.java
+++ src/main/java/api/concepts/APIConcepts/ReplaceAll.java
@@ -4,8 +4,8 @@ import groovy.transform.ASTTest;
 
 public class ReplaceAll {
     public static void main(String [] args){
-        String name = "vamsi123@#";
-        name = name.replaceAll("[^a-z]","").toUpperCase();
-        System.out.println("name is in capital letters : "name);
+        String name = "KASVIKA123@#";
+        name = name.replaceAll("[^a-z]","").toLowerCase();
+        System.out.println("name is in small letters : "name);
     }
 }
