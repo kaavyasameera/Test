86a4d859bec99c8765a14baa4a34ac2f1c3e6277
kaavyasameera <sajjakaavya@gmail.com>
2024-03-05 10:38:08 +0530

    commit for replaceall file name becomes vamsi

--- src/main/java/api/concepts/APIConcepts/ReplaceAll.java
+++ src/main/java/api/concepts/APIConcepts/ReplaceAll.java
@@ -4,7 +4,7 @@ import groovy.transform.ASTTest;
 
 public class ReplaceAll {
     public static void main(String [] args){
-        String name = "sameere123@#";
+        String name = "vamsi123@#";
         name = name.replaceAll("[^a-z]","").toUpperCase();
         System.out.println(name);
     }
